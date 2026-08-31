95c929c3cc96b96cba037b37e51b9fbfa1a4fe15
4n0m4ly <andrea.insinga.09@gmail.com>
2026-08-31 21:51:47 +0200

    Completed the function that handles the quote currency.

--- Cripto_analizer.py
+++ Cripto_analizer.py
@@ -8,9 +8,11 @@ api_key = os.getenv('API_KEY')
 
 import requests 
 
-print('Welcome')
+print('Welcome\n')
+      
 # Bisogna trovare un modo di accertare che la crypto inserita sia esistente
-# magari tramite una richiesta get
+# magari tramite una richiesta get e poi mettere una lista e dire se sta qui fai
+# i blocchi if seguenti
 
 crypto_list = []
 
@@ -18,12 +20,26 @@ def input_crypto():
     while True:
         crypto = input(
             'Which crypto do you want to trace? (ex. bitcoin, ethereum...),' \
-            'when you have done type "ok" to continue or "q" to quit the program.'
+                'when you have done type "ok" to continue or "q" to quit the '
+                    'program.\n'
             )
         if crypto == 'ok':
             return crypto_list
         elif crypto == 'q':
+            print('Bye!')
             break
         else:        
             crypto_list.append(crypto)
 
+# Lo stesso controllo vael anche per le valute di scambio
+
+def input_quote_currency():
+    while True:
+        quote_currency = input(
+            'Now please type the quote currency (ex. usd, eur...) or type "q" to ' \
+                'quit the program.\n')
+        if quote_currency == 'q':
+            print('Bye!')
+            break
+        else:
+            return quote_currency
