4bd6303a1adb25291021c1456a30010eff23d5d3
4n0m4ly <andrea.insinga.09@gmail.com>
2026-08-31 21:40:06 +0200

    Completed the function that asks and saves the crypto to trace.

--- Cripto_analizer.py
+++ Cripto_analizer.py
@@ -1,6 +1,29 @@
-import os
+# Da rimuovere per la versione 1.0
+import os 
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
-print(api_key)
+######################################
+
+import requests 
+
+print('Welcome')
+# Bisogna trovare un modo di accertare che la crypto inserita sia esistente
+# magari tramite una richiesta get
+
+crypto_list = []
+
+def input_crypto():
+    while True:
+        crypto = input(
+            'Which crypto do you want to trace? (ex. bitcoin, ethereum...),' \
+            'when you have done type "ok" to continue or "q" to quit the program.'
+            )
+        if crypto == 'ok':
+            return crypto_list
+        elif crypto == 'q':
+            break
+        else:        
+            crypto_list.append(crypto)
+
