a8103122fa41d3ee23c7732e681a9b62a68de15a
4n0m4ly <andrea.insinga.09@gmail.com>
2026-09-05 22:36:05 +0200

    Completed the program, added try-except blocks and fixed q and ok special inputs.

--- crypto_analyzer.py
+++ crypto_analyzer.py
@@ -7,83 +7,110 @@ api_key = os.getenv('API_KEY')
 ######################################
 
 import requests 
+import sys
 
-print('Welcome\n')
+print('Welcome...')
 
 crypto_list = []
 
 def input_crypto():
     while True:
         crypto = input(
-            'Which crypto do you want to trace? (ex. bitcoin, ethereum...).' \
+            'Which crypto do you want to trace? (ex. bitcoin, ethereum...). ' \
                 'When you have done type "ok" to continue or "q" to quit the '
                     'program.\n'
             )
-        url = f'https://api.coingecko.com/api/v3/coins/{crypto}'
 
-        params = {
-            'tickers': 'false',
-            'market_data': 'false',
-            'community_data': 'false',
-            'developer-data': 'false',
-            'sparkline': 'false'
-        }
+        if crypto == 'q':
+            print('Bye!')
+            sys.exit()
+        elif crypto == 'ok' and crypto_list:
+             return crypto_list
+        else:
+            url = f'https://api.coingecko.com/api/v3/coins/{crypto}'
 
-        headers = {
-            'x-cg-demo-api-key': api_key
-        }
+            params = {
+                'tickers': 'false',
+                'market_data': 'false',
+                'community_data': 'false',
+                'developer-data': 'false',
+                'sparkline': 'false'
+            }
 
-        response = requests.get(url, params=params, headers=headers)
-        if response.status_code == 200:
-            if crypto == 'ok':
-                return crypto_list
-            elif crypto == 'q':
-                print('Bye!')
-                break
-            else:        
-                crypto_list.append(crypto)
-        elif response.status_code == 404:
-            print('The crypto id that you provided is not available, if it exists please remember to use lowercase letters.')
+            headers = {
+                'x-cg-demo-api-key': api_key
+            }
+
+            try:
+                response = requests.get(url, params=params, headers=headers)
+            except requests.exceptions.ConnectionError:
+                print('Something is not working, check your internet connection or your API key and try again.')
+                sys.exit()
+            else:
+                if response.status_code == 200:        
+                    crypto_list.append(crypto)
+                elif response.status_code == 404:
+                    print('The crypto id that you provided is not available, if it exists please remember to use lowercase letters.')
+
 
 def input_quote_currency():
+
+    print('---------------------------')
+
     while True:
         quote_currency = input(
             'Now please type the quote currency (ex. usd, eur...) or type "q" to ' \
-                'quit the program.\n')
+                'quit the program.\n'
+            )
+
+        if quote_currency == 'q':
+            print('Bye!')
+            sys.exit()
+        else:
         
-        url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies'
+            url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies'
 
-        headers = {
-            'x-cg-demo-api-key': api_key
-        }
+            headers = {
+                'x-cg-demo-api-key': api_key
+            }
 
-        response = requests.get(url, headers=headers).json()
-        if quote_currency in response:
-            if quote_currency == 'q':
-                print('Bye!')
-                break
+            try:
+                response = requests.get(url, headers=headers).json()
+            except requests.exceptions.ConnectionError:
+                print('Something is not working, check your internet connection or your API key and try again.')
+                sys.exit()
             else:
-                return quote_currency
-        elif quote_currency not in response:
-            print('The quote currency that you provided is not available, if it exists please remember to use lowercase letters.')
+                if quote_currency in response:
+                    return quote_currency
+                elif quote_currency not in response:
+                    print('The quote currency that you provided is not available, if it exists please remember to use lowercase letters.')
 
 def get_data():
     crypto_list = input_crypto()
     quote_currency = input_quote_currency()
     print('Getting data...\n')
     for crypto in crypto_list:
+
         url = 'https://api.coingecko.com/api/v3/simple/price'
+
         params = {
             'ids': crypto,
             'vs_currencies': quote_currency
         }
+
         headers = {
             'x-cg-demo-api-key': api_key
         }
-        request = requests.get(url, params=params, headers=headers)
-        data = request.json()
-        for crypto_id in data.values():
-            for value in crypto_id.values():
-                print(f'- 1 {crypto} now is {value} {quote_currency}')
+
+        try:
+            request = requests.get(url, params=params, headers=headers)
+        except requests.exceptions.ConnectionError:
+            print('Something is not working, check your internet connection or your API key and try again.')
+            sys.exit()
+        else:
+            data = request.json()
+            for crypto_id in data.values():
+                for value in crypto_id.values():
+                    print(f'- 1 {crypto} now is {value} {quote_currency}')
 
 get_data()
